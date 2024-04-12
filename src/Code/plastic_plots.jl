eed9e0d9babbf39a5cfcf3f2099be66251ea5b86
CWillberg <christian.willberg@gmx.de>
2024-04-12 09:16:25 +0200

    new plots

--- src/Code/plastic_plots.jl
+++ src/Code/plastic_plots.jl
@@ -5,7 +5,7 @@ using LaTeXStrings
 u = [0,1,2]
 y1 = [0,1,1]
 y2 = [0,1,1.5]
-plot(u, [y1 y2], xticks = -10:20:10,yticks = -10:30:20, linewidth=2,label=["Elastisch plastisch" "Elastisch plastisch linear verfestigt" ], bottom_margin=50*mm)
+plot(u, [y1 y2], xticks = -10:20:10,yticks = -10:30:20, linewidth=2,label=["Elastisch plastisch" "Elastisch plastisch linear verfestigt" ], bottom_margin=5mm, left_margin = 5mm)
 # Labeling the axes
 xlabel!(L"Dehnung [$-$]")
 ylabel!(L"Spannung [$N/m^2$]")
